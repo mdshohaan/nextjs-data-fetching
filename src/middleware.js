0a5dfc9c82151a2a0254b4f71077861e479ba73d
mdshohaan <irfanulislamdu@gmail.com>
2025-02-13 12:36:19 +0600

    chore:Database func set

---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export const middleware = async (req) => {
+
+    const token = await getToken({ req });
+    console.log(token);
+    const isTokenOK = Boolean(token)
+    const isAdminUser = token?.role == 'admin'
+    const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/dashboard")
+
+    if (isAdminSpecificRoute && !isAdminUser) {
+        const callbackUrl = encodeURIComponent(req.nextUrl.pathname)
+        return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${callbackUrl}`, req.url))
+    }
+
+
+    return NextResponse.next()
+}
